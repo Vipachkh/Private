4820ad2e732ceff98d025af76b919fa9602d82e6
Vipachkh <vipachkh@example.com>
2026-04-21 14:41:19 +0700

    Configure vercel.json for monorepo deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
-    return [
+    return process.env.NODE_ENV === "development" ? [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8001/api/:path*", // Proxy to internal FastAPI
+        destination: "http://127.0.0.1:8001/api/:path*", // Proxy to internal FastAPI only locally
       },
-    ];
+    ] : [];
   },
 };
 export default nextConfig;
